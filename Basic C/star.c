7894495a2ae660ddf3d0b3c58cb94d52b1636ff3
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-08-02 19:49:05 +0600

    Add files via upload

---
+++ Basic C/star.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main (){
+    int n;
+    scanf("%d",&n);
+    printf("**********\n");
+    for(int i=1;i<=n-2;i++){
+        printf("*        *\n");
+    }
+    printf("**********\n");
+    return 0;
+}
