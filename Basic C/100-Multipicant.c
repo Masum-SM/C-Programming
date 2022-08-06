9e2cb6add98708af1b8e9693d104d51f00fe41ee
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-08-06 15:08:46 +0600

    Add files via upload

---
+++ Basic C/100-Multipicant.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n,count=0;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        if(n%i == 0){
+            printf("%d\n",i);
+            count++;
+        }
+    }
+    printf("Number of divisor of n is %d",count);
+}
