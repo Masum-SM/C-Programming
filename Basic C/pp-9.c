404fab75bec68481b489e484828a624a02694479
Unus Masum <86046642+Masum-SM@users.noreply.github.com>
2022-08-08 23:02:09 +0600

    Add files via upload

---
+++ Basic C/pp-9.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+
+/*
+int main(){
+    int n = 100,sum =0;
+    for(int i = 0; i<=n; i++){
+        sum = sum + i;
+    }
+    printf("summation = %d", sum);
+}
+*/
+
+/*
+int main(){
+    int n = 29,sum =0;
+    for(int i = 0; i<=n; i++){
+        if(i % 2 != 0){
+            sum = sum + i;
+            printf("%d \n",i);
+        }
+
+    }
+    printf("summation = %d", sum);
+}
+
+*/
+
+
+/*
+int main(){
+    int n = 30,sum =0;
+    for(int i = 50; i>n; i--){
+
+            sum = sum + i;
+            printf("%d \n",i);
+
+
+    }
+    printf("summation = %d", sum);
+}
+
+*/
+
+int main(){
+    int n = 30,sum =0;
+    for(int i = 50; i>n; i--){
+
+            sum = sum + i;
+            printf("%d \n",i);
+
+
+    }
+    printf("summation = %d", sum);
+}
